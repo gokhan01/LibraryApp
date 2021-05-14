a7c71ae4770e4f84b2e8896ab72ca6dd45ee7052
inveon-gokhanuyar <79637891+inveon-gokhanuyar@users.noreply.github.com>
2021-05-14 23:05:09 +0300

    v4

--- LibraryApp/Controllers/HomeController.cs
+++ LibraryApp/Controllers/HomeController.cs
@@ -10,7 +10,6 @@ using System.Threading.Tasks;
 
 namespace LibraryApp.Controllers
 {
-    [AllowAnonymous]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
